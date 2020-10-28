bf27a2fa72752676aa6fe664accb74ba535cbe50
D1eRise <48651064+D1eRise@users.noreply.github.com>
2020-10-29 00:59:46 +0300

    footer.php (правка табуляций)

--- templates/footer.php
+++ templates/footer.php
@@ -11,28 +11,28 @@
 				requestData.confPass = $('#inputPassword2').val();
 
 				$.ajax({
-		            url: 'reg.php',
-		            type: 'POST',
-		            data: requestData,
-		            success: function(res){
-		                let result = JSON.parse(res);
-		                if (result.status === "error") {
-			                if (!$("#error").is(":visible")) 
-			                	$("#error").show();
-			                $("#error").html(result.message);
-		                } else if (result.status === "success") {
-		                	if ($("#error").is(":visible")) 
-		                		$("#error").hide();
-		                	$("form").hide();
-		                	$("#success").show();
-		                } 
-		            },
-		            error: function(e){
-		                console.log(e)  
-		            }
-		        });
-			})
-		})
+				    url: 'reg.php',
+				    type: 'POST',
+				    data: requestData,
+				    success: function(res){
+					let result = JSON.parse(res);
+					if (result.status === "error") {
+						if (!$("#error").is(":visible")) 
+							$("#error").show();
+						$("#error").html(result.message);
+					} else if (result.status === "success") {
+						if ($("#error").is(":visible")) 
+							$("#error").hide();
+						$("form").hide();
+						$("#success").show();
+					} 
+				    },
+				    error: function(e){
+					console.log(e)  
+				    }
+				});
+			});
+		});
 	</script>
 	</body>
 </html>
